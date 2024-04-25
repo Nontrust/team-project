110b6199072159c868cdac85e860437fe114e910
Nontrust <deed1515@naver.com>
2024-04-25 15:54:45 +0900

    :wrench: [config] 서버포트 설정, ide 경로 설정

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,11 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    VueDevTools(),
-  ],
+  server:{
+    strictPort: true,
+    port: 3_000,
+  },
+  plugins: [vue(), VueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
