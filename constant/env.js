a68972d4d1504ac75cf01b0bc1234add1abd8638
Nontrust <deed1515@naver.com>
2024-04-30 14:11:14 +0900

    :wrench: [env] env 파일과 매칭될 env.js 파일 추가

---
+++ constant/env.js
@@ -0,0 +1,8 @@
+export const API_PORT = import.meta.env.API_PORT
+export const FRONT_PORT = import.meta.env.FRONT_PORT
+
+export const FRONT_BASE_HOST = import.meta.env.FRONT_BASE_HOST
+export const API_BASE_HOST = import.meta.env.API_BASE_HOST
+
+export const FRONT_BASE_URL = `${FRONT_BASE_HOST}:${FRONT_PORT}`
+export const API_BASE_URL = `${API_BASE_HOST}:${API_PORT}`
--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,22 @@
 import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
-import dotenv from 'dotenv'
 
-// https://vitejs.dev/config/
-dotenv.config()
-export default defineConfig({
-  server: {
-    strictPort: true,
-    port: process.env.VITE_FRONT_PORT
-  },
-  plugins: [vue(), VueDevTools()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+
+
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+  return defineConfig({
+    server: {
+      strictPort: true,
+      port: process.env.VITE_FRONT_PORT | 3000
+    },
+    plugins: [vue(), VueDevTools()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./', import.meta.url))
+      }
     }
-  }
-})
+  })
+}
